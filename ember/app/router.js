a873aa138bd9adcf26ad9666d83c825dd6d6c9da
Dave-Choi <daaave@gmail.com>
2014-06-04 16:29:44 -0400

    Change solveMethod resource to be a puzzle child
    Also modified the names of the path params, and removed unnecessary
    route definitions, using default behavior.

--- ember/app/router.js
+++ ember/app/router.js
@@ -5,11 +5,11 @@ var Router = Ember.Router.extend({
 Router.map(function(){
 	this.resource("stackedAreaTest");
 	this.resource("puzzles");
-	this.resource("puzzle", { path: "puzzles/:id" });
-
-	this.resource("solveMethod", { path: "solveMethods/:id" }, function(){
-		this.route("stats");
-		this.route("timer");
+	this.resource("puzzle", { path: "puzzles/:puzzle_id" }, function(){
+		this.resource("solveMethod", { path: "solveMethods/:solveMethod_id" }, function(){
+			this.route("stats");
+			this.route("timer");
+		});
 	});
 });
 
