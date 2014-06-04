ced5f36332519fa4c92a1f5b24cd71dbc493dc20
Dave-Choi <daaave@gmail.com>
2014-06-04 16:38:14 -0400

    Remove model fixtures

--- ember/app/models/solve.js
+++ ember/app/models/solve.js
@@ -1,6 +1,6 @@
 Ember.Inflector.inflector.irregular("solve", "solves");
 
-var Solve = DS.Model.extend({
+export DS.Model.extend({
 	scramble: DS.attr("string"),
 
 	stepResults: DS.hasMany("stepResult", { async: true }),
@@ -22,24 +22,3 @@ var Solve = DS.Model.extend({
 		this.set("totalTime", total);
 	}.observes("stepResults.@each.time")
 });
-
-function generateFixtures(count){
-	var i;
-	var fixtures = [];
-	for(i = 1; i <= count; i++){
-		fixtures.push({
-			id: i,
-			scramble: "FIXTURE SCRAMBLE",
-			solveMethod: 1,
-			user: 1
-		});
-	}
-
-	return fixtures;
-}
-
-Solve.reopenClass({
-	FIXTURES: generateFixtures(100)
-});
-
-export default Solve;
--- ember/app/models/user.js
+++ ember/app/models/user.js
@@ -1,14 +1,3 @@
-var User = DS.Model.extend({
+export default DS.Model.extend({
 	email: DS.attr("string")
 });
-
-User.reopenClass({
-	FIXTURES: [
-		{
-			"id": 1,
-			"email": "example@example.com"
-		}
-	]
-});
-
-export default User;
