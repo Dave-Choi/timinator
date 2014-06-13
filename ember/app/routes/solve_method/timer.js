fc4dce0c0787eddbc6de7def81763aff1466678e
Dave-Choi <daaave@gmail.com>
2014-06-13 04:10:13 -0400

    Set SolveController model in Timer route

--- ember/app/routes/solve_method/timer.js
+++ ember/app/routes/solve_method/timer.js
@@ -1,3 +1,6 @@
 export default Ember.Route.extend({
-
+	setupController: function(controller){
+		var newSolve = controller.newSolve();
+		this.controllerFor("solve").set("model", newSolve);
+	}
 });
