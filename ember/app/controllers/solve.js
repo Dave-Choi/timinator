4aba9b978ed5519c1b42dc2debe04d7c1c29bb88
Dave-Choi <daaave@gmail.com>
2014-06-13 04:04:56 -0400

    Add SolveController

---
+++ ember/app/controllers/solve.js
@@ -0,0 +1,35 @@
+export default Ember.ObjectController.extend({
+	currentStepIndex: 0,
+	numSteps: Ember.computed.alias("solveMethod.steps.length"),
+
+	currentStep: function(){
+		var index = this.get("currentStepIndex");
+		return this.get("solveMethod.steps").objectAt(index);
+	}.property("currentStepIndex", "solveMethod.steps"),
+
+	advance: function(time){
+		this.addTime(time);
+		
+		var index = this.get("currentStepIndex");
+		var numSteps = this.get("numSteps");
+
+		if(index < numSteps - 1){
+			this.incrementProperty("currentStepIndex");
+			return true;
+		}
+		this.set("currentStepIndex", 0);
+		return false;
+	},
+
+	addTime: function(time){
+		var stepResult = this.store.createRecord("stepResult", {
+			solve: this.get("model"),
+			step: this.get("currentStep"),
+			time: time
+		});
+
+		console.log(stepResult);
+
+		this.get("stepResults").addObject(stepResult);
+	}
+});
