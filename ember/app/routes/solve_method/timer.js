b763b59a002e2354f7a311e9c365eb9059147694
Dave-Choi <daaave@gmail.com>
2014-07-01 16:30:39 -0400

    Send start and stop keyboard shortcut actions from timer route to controller

--- ember/app/routes/solve_method/timer.js
+++ ember/app/routes/solve_method/timer.js
@@ -1,6 +1,16 @@
 export default Ember.Route.extend({
 	shortcuts: {
-		"space": "step"
+		"space": "step",
+		"esc": "stop"
+	},
+
+	actions: {
+		step: function(){
+			this.controllerFor("solveMethod.timer").send("step");
+		},
+		stop: function(){
+			this.controllerFor("solveMethod.timer").send("stop");
+		}
 	},
 
 	setupController: function(controller){
