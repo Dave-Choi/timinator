2539b5acf353e883eb3c327d61eabf2a0bd5a078
Dave-Choi <daaave@gmail.com>
2014-07-01 18:47:01 -0400

    Add aggregate time CPs to SolvesController

--- ember/app/controllers/solves.js
+++ ember/app/controllers/solves.js
@@ -1,3 +1,8 @@
 export default Ember.ArrayController.extend({
+	totalTimes: Ember.computed.mapBy("model", "totalTime"),
+	totalTime: Ember.computed.sum("totalTimes"),
 
+	totalMean: function(){
+		return (this.get("totalTime") / this.get("length")) || 0;
+	}.property("totalTime", "length")
 });
